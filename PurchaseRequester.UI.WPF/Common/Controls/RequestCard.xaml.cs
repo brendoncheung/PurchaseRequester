7261ddf4e07e4374854e6dbccf73c32a21213e85
Wing Sun Cheung <brendon.cheung@outlook.com>
2022-05-24 16:50:17 -0400

    need to fix data binding

---
+++ PurchaseRequester.UI.WPF/Common/Controls/RequestCard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PurchaseRequester.UI.WPF.Common.Controls
+{
+    /// <summary>
+    /// Interaction logic for RequestCard.xaml
+    /// </summary>
+    public partial class RequestCard : UserControl
+    {
+        public RequestCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PurchaseRequester.UI.WPF/Requests/ViewModel/RequestListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using PurchaseRequester.Domain.Requests;
+using PurchaseRequester.UI.WPF.Repository.Requests;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PurchaseRequester.UI.WPF.Requests.ViewModel
+{
+    public class RequestListViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Request> Requests;
+        private readonly RequestRepository repository;
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RequestListViewModel(ObservableCollection<Request> request, RequestRepository repository)
+        {
+            this.Requests = request;
+            this.repository = repository;
+        }
+
+        public void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        public void LoadAllRequests()
+        {
+            Requests.Clear();
+            repository.GetRequests().ForEach(r => Requests.Add(r));
+        }
+         
+    }
+}
